085852219b32121059012a0d4076649467380dbb
shuGeorgio <dev.shudiscrete@gmail.com>
2020-05-19 11:13:53 +0900

    Update Titles

--- pages/game.js
+++ pages/game.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Imagine Debater Game</title>
+        <title>Imagine Debater | Game</title>
         <link rel="icon" href="/imagine-logo.png" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
         <link href="/lib/css/style.css" rel="stylesheet" />
